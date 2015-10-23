f30f5a17dab8518e94eca3cd2c775838a317729d
wearle <ola.anderssn@gmail.com>
2015-10-23 16:13:56 +0100

    testCommit

---
+++ MyWebAppTest/MyWebAppTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWebAppTest.Startup))]
+namespace MyWebAppTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
